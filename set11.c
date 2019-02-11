fe2bf7702b408b4fe1718aae6a4542269fa6234a
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-02-11 16:29:07 +0530

    Create set11.c

---
+++ set11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+if(a<0)
+{
+printf("negative)
+}
+ifelse(a=0)
+{
+printf("Zero);
+}
+else
+{
+printf("positive");
+}
+getch();
+}
