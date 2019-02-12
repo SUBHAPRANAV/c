3a4132fd7d50c5a9593fa006b1c6bceaede9e8a1
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-02-12 23:57:47 +0530

    Create set14.c

---
+++ set14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+char ch;
+scanf("%c",&ch);
+if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+{
+printf("Alphabet");
+}
+else
+{
+printf("Not Alphabet");
+}
+return 0;
+}
