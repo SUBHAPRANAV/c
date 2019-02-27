c2a2081c04ea798c819c462f9d169107317d8d09
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-02-27 16:06:46 +0530

    Update set2prgm13.c

--- set2prgm13.c
+++ set2prgm13.c
@@ -1,26 +1,20 @@
 #include<studio.h>
 int main()
 {
-int n,i,flag=0;
-scanf(""%d",&n);
-for(i=0;i<=n/2;i++)
+int n,i,count=0;
+scanf("%d",&n);
+for(i=2;i<n;i++)
 {
 if(n%1==0)
-{
-flag=1;
-break;
+count++;
 }
-}
-else
-{
-if(flag==0)
+if (count==0)
 {
 printf("Yes");
 }
 else
 {
 printf("No");
-}
 }
 return 0;
 }
