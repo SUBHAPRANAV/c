4f1ffaf211ce007e6e9e767ff8294076366cbb5c
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-02-27 15:42:17 +0530

    Create set2prgm12.c

---
+++ set2prgm12.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+void main();
+int i;
+char a[50],b[50];
+scanf("%s",a);
+strcpy(b,a);p
+strrev(a);
+printf("%s",a);
+i=strcmp(a,b);
+prinbtf("%d",i);
+if(i==0)
+{
+printf("palindrome");
+}
+else
+{
+printf("Not palindrome");
+}
+return 0;
+}
