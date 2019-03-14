238744a8494ff85a5cd9fd0a1f4a6249e2b69d4a
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-03-14 16:20:47 +0530

    Create set4prgm32.c

---
+++ set4prgm32.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char s[200];
+int c=0,i;
+scanf("%[^\n]s", s);
+for (i = 0;s[i] != '\0';i++)
+    {
+        if (s[i] == ' ')
+            c++;    
+    }
+    printf("%d\n", c + 1);
+}
