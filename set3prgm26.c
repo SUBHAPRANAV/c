c9d0769d7b935e21e35b7f74660064568ccc0878
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-03-14 15:53:29 +0530

    Create set3prgm26.c

---
+++ set3prgm26.c
@@ -0,0 +1,30 @@
+# include<stdio.h>
+int main()
+{
+int n,ar[n],i,j,t;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&ar[i]);
+	
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(ar[j]<ar[i])
+{
+t=ar[i];
+ar[i]=ar[j];
+ar[j]=t;
+}
+	
+}
+	
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t",ar[i]);
+}
+return 0;
+}
