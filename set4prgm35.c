be88940c71600eef8dff10caccc05c60fe6c49eb
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-03-25 16:06:53 +0530

    Create set4prgm35.c

---
+++ set4prgm35.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[1000];
+	int n,i,count=0;
+	scanf("%s",&a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]>='0' && a[i]<='9')
+	    {
+	        count++;
+	    }
+	}
+	printf("%d",count);
+}
