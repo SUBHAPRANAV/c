adb949716172ad834c66408132b6f791bf2ec2de
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-03-25 16:33:33 +0530

    Create set5prgm42.c

---
+++ set5prgm42.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char a[100],b[100];
+	int i,n,n1;
+	scanf("%s",&a);
+	scanf("%s",&b);
+	n=strlen(a);
+	n1=strlen(b);
+    if(n==n1)
+    {
+        printf("%s",b);
+    }
+    else if(n>n1)
+    {
+        printf("%s",a);
+    }
+    else
+    {
+        printf("%s",b);
+    }
+ }
