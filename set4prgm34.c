68fd10d1e89fedf235f5acefaecdf2976e7e7469
SUBHAPRANAV <47105651+SUBHAPRANAV@users.noreply.github.com>
2019-03-25 16:16:48 +0530

    Create set4prgm34.c

---
+++ set4prgm34.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[1000];
+	int n,i,count=1;
+	scanf("%s",&a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]=='.'&& a[i+1]!='\0')
+	    {
+	        count++;
+	    }
+	}
+	printf("%d",count);
+}
